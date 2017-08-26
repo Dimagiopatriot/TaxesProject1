440a9bd939821be7d93f99134c8a05214bc732dc
Dimagio <sdmitriy3758@gmail.com>
2017-08-26 19:03:14 +0300

    add utils

---
+++ src/main/java/controller/utils/Constants.java
@@ -0,0 +1,12 @@
+package controller.utils;
+
+public interface Constants {
+
+    String WORK_TAX_NAME = "work";
+    String REWARD_TAX_NAME = "reward";
+    String PROPERTY_TAX_NAME = "property";
+    String GIFTS_TAX_NAME = "gifts";
+    String TRANSFER_TAX_NAME = "transfer";
+    String CHILDREN_PRIVILEGES_TAX_NAME = "children_privileges";
+    String MATERIAL_AID_TAX_NAME = "material_aid";
+}
