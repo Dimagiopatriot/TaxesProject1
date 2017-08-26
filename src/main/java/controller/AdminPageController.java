715d7790670394c7335129f674f936adef154ab4
Dimagio <sdmitriy3758@gmail.com>
2017-08-26 19:19:03 +0300

    fill admin controller

--- src/main/java/controller/AdminPageController.java
+++ src/main/java/controller/AdminPageController.java
@@ -1,23 +1,57 @@
 package controller;
 
+import model.dao.TaxDaoInterface;
+import model.dao.impl.TaxDao;
+import model.entities.taxes.Tax;
+import model.entities.taxes.TaxBuilder;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class AdminPageController extends HttpServlet {
+import static controller.utils.Constants.*;
 
-    private final static String WORK_TAX_NAME = "work";
-    private final static String REWARD_TAX_NAME = "reward";
-    private final static String PROPERTY_TAX_NAME = "property";
-    private final static String GIFTS_TAX_NAME = "gifts";
-    private final static String TRANSFER_TAX_NAME = "transfer";
-    private final static String CHILDREN_PRIVILEGES_TAX_NAME = "children_privileges";
-    private final static String MATERIAL_AID_TAX_NAME = "material_aid";
+public class AdminPageController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TaxDaoInterface taxDao = new TaxDao();
 
+        Tax work = createTax(WORK_TAX_NAME, req);
+        Tax reward = createTax(REWARD_TAX_NAME, req);
+        Tax property = createTax(PROPERTY_TAX_NAME, req);
+        Tax gifts = createTax(GIFTS_TAX_NAME, req);
+        Tax transfer = createTax(TRANSFER_TAX_NAME, req);
+        Tax childrenPrivileges = createTax(CHILDREN_PRIVILEGES_TAX_NAME, req);
+        Tax materialAid = createTax(MATERIAL_AID_TAX_NAME, req);
+
+        updateTaxInDatabase(work, taxDao);
+        updateTaxInDatabase(reward, taxDao);
+        updateTaxInDatabase(property, taxDao);
+        updateTaxInDatabase(gifts, taxDao);
+        updateTaxInDatabase(transfer, taxDao);
+        updateTaxInDatabase(childrenPrivileges, taxDao);
+        updateTaxInDatabase(materialAid, taxDao);
+        RequestDispatcher rd = req.getRequestDispatcher("main_admin.jsp");
+        rd.forward(req, resp);
     }
+
+    private Tax createTax(String type,HttpServletRequest request) {
+        return new TaxBuilder()
+                .setName(type)
+                .setTaxPercent(Double.parseDouble(request.getParameter(type)))
+                .createTax();
+    }
+
+    private void updateTaxInDatabase(Tax tax, TaxDaoInterface taxDao){
+        if (!taxDao.updateTaxByName(tax)){ insertTaInDatabase(tax, taxDao);}
+    }
+
+    private void insertTaInDatabase(Tax tax, TaxDaoInterface taxDao){
+        taxDao.insert(tax);
+    }
+
 }
