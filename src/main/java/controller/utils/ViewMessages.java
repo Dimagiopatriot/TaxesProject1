e8ff3db890d0cf91e11893dd8211d822265d42cf
Dimagio <sdmitriy3758@gmail.com>
2017-09-01 02:57:39 +0300

    add new messages

--- src/main/java/controller/utils/ViewMessages.java
+++ src/main/java/controller/utils/ViewMessages.java
@@ -5,13 +5,14 @@ public interface ViewMessages {
     String EMAIL_AND_PASSWORD_ERROR = "Невірно вказаний логін або пароль!";
     String REGISTER_CONGRATULATION = "Ви зареєстровані!!";
     String REGISTER_ERROR = "На жаль, виникла помилка. Спробуйте ще раз.";
+    String USER_REGISTERED_BEFORE_ERROR = "Такий користувач вже зареєстрований";
 
-    String WORK_TAX_FOR_PAYING = "Ваш податок від заробітку :";
-    String WORK_ADD_TAX_FOR_PAYING = "Ваш податок від додаткового заробітку :";
-    String REWARD_TAX_FOR_PAYING = "Ваш податок від авторських вінагороджень :";
-    String PROPERTY_TAX_FOR_PAYING = "Ваш податок від продажі майна :";
-    String GIFTS_TAX_FOR_PAYING = "Ваш податок від отриманих подарунків :";
-    String TRANSFER_TAX_FOR_PAYING = "Ваш податок від переказів :";
-    String CHILDREN_PREVILEGES_TAX_FOR_PAYING = "Ваш податок від дитячих пільг :";
-    String MATERIAL_AID_TAX_FOR_PAYING = "Ваш податок від матеріальної допомоги :";
+    String WORK_TAX_FOR_PAYING = "Ваш податок від заробітку ";
+    String WORK_ADD_TAX_FOR_PAYING = "Ваш податок від додаткового заробітку ";
+    String REWARD_TAX_FOR_PAYING = "Ваш податок від авторських винагороджень ";
+    String PROPERTY_TAX_FOR_PAYING = "Ваш податок від продажі майна ";
+    String GIFTS_TAX_FOR_PAYING = "Ваш податок від отриманих подарунків ";
+    String TRANSFER_TAX_FOR_PAYING = "Ваш податок від переказів ";
+    String CHILDREN_PRIVILEGES_TAX_FOR_PAYING = "Ваш податок від дитячих пільг ";
+    String MATERIAL_AID_TAX_FOR_PAYING = "Ваш податок від матеріальної допомоги ";
 }
